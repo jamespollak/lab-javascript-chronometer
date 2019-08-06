300b725cf963cd53a2e40caafa7f8ba34f866469
James Pollak <james.pollak@htomail.com>
2019-08-06 21:00:08 +0100

    adding work done so far on Chronometer

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,6 +1,20 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
+    this.addOneToTimer = this.addOneToTimer.bind(this)
+
+  }
+  startClick() {
+    this.intervalId = window.setInterval( this.addOneToTimer, 1000);
+    const newTime = document.getElementById("secUni") 
+    newTime.innerText = this.currentTime
+  }
+
+  addOneToTimer() {
+    this.currentTime = this.currentTime +1;
+  }
+
   // getMinutes() {}
   // getSeconds() {}
   // twoDigitsNumber() {}
@@ -8,3 +22,21 @@ class Chronometer {
   // resetClick() {}
   // splitClick() {}
 }
+
+//var chrono = new Chronometer();
+
+/* 
+  chrono = {
+    currentTime: 0,
+    intervalId: 1000,
+    startClick: function() {
+      chrono.currentTime = chrono.currentTime + 1
+    }
+  }
+*/
+chrono.currentTime
+chrono.startClick()
+
+/*var newtime = document.getElementById("milDec").innerText
+console.log(newtime)
+debugger*/
